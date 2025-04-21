d52a340c87807e681b1ebbcfc483bc7aaa9f0683
rspns1 <ruziya.g@gmail.com>
2025-04-21 10:33:26 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
-function myFunction(){
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event){
-    if(!event.target.matches('.dropbtn')){
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
-        for(i=0; i< dropdowns.length; i++){
+        for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')){
+            if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
         }
     }
 }
 
-function myFunction2(){
+function myFunction2() {
     document.getElementById("myDropdown2").classList.toggle("show2");
 }
 
-window.onclick = function(event){
-    if(!event.target.matches('.dropbtn2')){
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn2')) {
         var dropdowns = document.getElementsByClassName("dropdown-content2");
         var i;
-        for(i=0; i< dropdowns.length; i++){
+        for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show2')){
+            if (openDropdown.classList.contains('show2')) {
                 openDropdown.classList.remove('show2');
             }
         }
@@ -34,15 +34,30 @@ window.onclick = function(event){
 
 var acc = document.getElementsByClassName("accordion");
 var i;
-for(i = 0; i< acc.length; i++){
-    acc[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block"){
+        if (panel.style.display === "block") {
             panel.style.display = "none";
-        
-    }else{
-        panel.style.display = "block";
+
+        } else {
+            panel.style.display = "block";
+        }
+    })
+}
+
+let btns = document.querySelectorAll('.btn')
+let texts = document.querySelectorAll('.text')
+let infons = document.querySelectorAll('.info')
+
+btns.forEach((btn, index) => {
+    btn.onclick = () => {
+        document.querySelector('.btn.active').classList.remove('active');
+        btn.classList.add('active')
+        document.querySelector('.text.active').classList.remove('active')
+        texts[index].classList.add('active')
+        document.querySelector('.info.active').classList.remove('active')
+        infons[index].classList.add('active')
     }
 })
-}
